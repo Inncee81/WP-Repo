2298132faf1752ff85476cdde6f690250d9323db
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 12:19:07 -0400

    Initialize refactor-integration-examples

---
+++ config/grunt/task-config/notify-slack.js
@@ -0,0 +1,7 @@
+// Custom task
+module.exports = {
+	rc: {
+		slackToken: process.env.SLACK_TEST_CHANNEL_TOKEN,
+		message: "<!subteam^S01CRBFCFAS> <!subteam^S01FHUB56RK> An RC has been created: ",
+	},
+};
