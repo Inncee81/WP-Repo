be92341816ef0e67ff3bd62e2cce633cf0b943e6
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 12:49:25 -0400

    Initialize fix-src-presenters

---
+++ src/presenters/webmaster/bing-presenter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Yoast\WP\SEO\Presenters\Webmaster;
+
+use Yoast\WP\SEO\Presenters\Abstract_Indexable_Tag_Presenter;
+
+/**
+ * Presenter class for the Bing Webmaster verification setting.
+ */
+class Bing_Presenter extends Abstract_Indexable_Tag_Presenter {
+
+	/**
+	 * The tag format including placeholders.
+	 *
+	 * @var string
+	 */
+	protected $tag_format = '<meta name="msvalidate.01" content="%s" />';
+
+	/**
+	 * Retrieves the webmaster tool site verification value from the settings.
+	 *
+	 * @return string $verification_value The webmaster tool site verification value.
+	 */
+	public function get() {
+		$verify = $this->helpers->options->get( 'msverify', '' );
+		if( is_string( $verify ) ) {
+			return $verify;
+		}
+
+		return '';
+	}
+}
