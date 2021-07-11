1ae237c4e2600ee1823ef6155cd3ffbdb464d7d3
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 13:11:31 -0400

    Initialize LINGO-864-fix-meta-description-excerpt

---
+++ packages/yoastseo/src/languageProcessing/languages/nb/Researcher.js
@@ -0,0 +1,41 @@
+import { languageProcessing } from "yoastseo";
+const { AbstractResearcher } = languageProcessing;
+
+// All config
+import firstWordExceptions from "./config/firstWordExceptions";
+import functionWords from "./config/functionWords";
+import transitionWords from "./config/transitionWords";
+import twoPartTransitionWords from "./config/twoPartTransitionWords";
+
+// All helpers
+import getStemmer from "./helpers/getStemmer";
+
+/**
+ * The researches contains all the researches
+ */
+export default class Researcher extends AbstractResearcher {
+	/**
+	 * Constructor
+	 * @param {Paper} paper The Paper object that is needed within the researches.
+	 * @constructor
+	 */
+	constructor( paper ) {
+		super( paper );
+
+		// Delete Flesch Reading Ease research since Norwegian doesn't have the support for it
+		delete this.defaultResearches.getFleschReadingScore;
+		delete this.defaultResearches.getPassiveVoiceResult;
+
+		Object.assign( this.config, {
+			language: "nb",
+			functionWords,
+			firstWordExceptions,
+			transitionWords,
+			twoPartTransitionWords,
+		} );
+
+		Object.assign( this.helpers, {
+			getStemmer,
+		} );
+	}
+}
