3246bfa709e16ca782225e4782924271ae2d72d0
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 11:36:54 -0400

    Initialize feature/test-yoast-dist-deploy

---
+++ scripts/deploy_to_dist.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+######################
+## Deployment script #
+######################
+## arg 1: git tag   ##
+## arg 2: repo-name ##
+######################
+
+set -ex
+
+if [ -z "$1" ]; then
+    echo 'The first argument should be the version you want to deploy to dist.'
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo 'The second argument should be the repo name.'
+    exit 1
+fi
+
+# Repo to deploy to:
+USER="Yoast-dist"
+REPO=$2
+REPO_URL="git@github.com:$USER/$REPO.git"
+
+# Get the latest tag.
+lastTag=$1
+branch="master"
+mainDir=$(pwd)
+
+if [[ $lastTag =~ ^feature/* ]]; then
+  branch=$lastTag
+fi
+
+# Clone the dist repo.
+rm -rf ./dist-repo
+git clone ${REPO_URL} dist-repo
+
+# Copy the git folder with the entire history.
+cp -r ./dist-repo/.git ./artifact-composer
+
+# Navigate to the to be committed folder.
+cd ./artifact-composer
+git checkout $branch 2>/dev/null || git checkout -b $branch
+
+# Commit the files.
+git add -A
+git commit -m "Release ${lastTag}"
+
+# If it's a feature branch.
+if [[ ! $lastTag =~ ^feature/* ]]; then
+   # Tag the commit.
+  git tag ${lastTag}
+fi
+
+# Push to remote.
+git push -u origin $branch --tags
