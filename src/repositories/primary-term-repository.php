7bea6b049f8b8c562ede742b0a9d0b75143e5528
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 11:50:54 -0400

    Initialize texturize-and-convert-chars-for-titles

---
+++ src/repositories/primary-term-repository.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Yoast extension of the Model class.
+ *
+ * @package Yoast\YoastSEO\ORM\Repositories
+ */
+
+namespace Yoast\WP\SEO\Repositories;
+
+use Yoast\WP\SEO\Models\Primary_Term;
+use Yoast\WP\SEO\ORM\ORMWrapper;
+use Yoast\WP\SEO\ORM\Yoast_Model;
+
+/**
+ * Class Primary_Term_Repository
+ *
+ * @package Yoast\WP\SEO\ORM\Repositories
+ */
+class Primary_Term_Repository {
+
+	/**
+	 * Starts a query for this repository.
+	 *
+	 * @return ORMWrapper
+	 */
+	public function query() {
+		return Yoast_Model::of_type( 'Primary_Term' );
+	}
+
+	/**
+	 * Retrieves a primary term by a post ID and taxonomy.
+	 *
+	 * @param int    $post_id     The post the indexable is based upon.
+	 * @param string $taxonomy    The taxonomy the indexable belongs to.
+	 * @param bool   $auto_create Optional. Creates an indexable if it does not exist yet.
+	 *
+	 * @return Primary_Term|null Instance of a primary term.
+	 */
+	public function find_by_post_id_and_taxonomy( $post_id, $taxonomy, $auto_create = true ) {
+		/** @var Primary_Term $primary_term */
+		$primary_term = $this->query()
+							 ->where( 'post_id', $post_id )
+							 ->where( 'taxonomy', $taxonomy )
+							 ->find_one();
+
+		if ( $auto_create && ! $primary_term ) {
+			$primary_term = $this->query()->create();
+		}
+
+		return $primary_term;
+	}
+}
---
+++ src/repositories/seo-meta-repository.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Yoast extension of the Model class.
+ *
+ * @package Yoast\YoastSEO\ORM\Repositories
+ */
+
+namespace Yoast\WP\SEO\Repositories;
+
+use Yoast\WP\SEO\ORM\ORMWrapper;
+use Yoast\WP\SEO\ORM\Yoast_Model;
+
+/**
+ * Class SEO_Meta_Repository
+ *
+ * @package Yoast\WP\SEO\ORM\Repositories
+ */
+class SEO_Meta_Repository {
+
+	/**
+	 * Starts a query for this repository.
+	 *
+	 * @return ORMWrapper
+	 */
+	public function query() {
+		return Yoast_Model::of_type( 'SEO_Meta' );
+	}
+
+	/**
+	 * Finds the SEO meta for given post.
+	 *
+	 * @param int $post_id The post ID.
+	 *
+	 * @return \Yoast\WP\SEO\Models\SEO_Meta The SEO meta.
+	 */
+	public function find_by_post_id( $post_id ) {
+		return $this->query()
+					->where( 'object_id', $post_id )
+					->find_one();
+	}
+}
