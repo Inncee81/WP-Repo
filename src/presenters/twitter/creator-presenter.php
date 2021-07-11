020295ab1d2d2366eef92b472e73c54f86db1f00
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 12:54:25 -0400

    Initialize 15574-add-yoast-json-head

---
+++ src/presenters/twitter/creator-presenter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Yoast\WP\SEO\Presenters\Twitter;
+
+use Yoast\WP\SEO\Presenters\Abstract_Indexable_Tag_Presenter;
+
+/**
+ * Presenter class for the Twitter creator.
+ */
+class Creator_Presenter extends Abstract_Indexable_Tag_Presenter {
+
+	const NAME = 'twitter:creator';
+
+	/**
+	 * Gets the raw value of a presentation.
+	 *
+	 * @return string The raw value.
+	 */
+	public function get() {
+		return $this->presentation->twitter_creator;
+	}
+}
