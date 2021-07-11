efda236ea0bb839608f0a03cb3b80e826b41ee9f
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 11:49:20 -0400

    Initialize fix-ruckusing-charset-config

---
+++ src/models/seo-links.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Model for the SEO Meta table.
+ *
+ * @package Yoast\YoastSEO\Models
+ */
+
+namespace Yoast\WP\SEO\Models;
+
+use Yoast\WP\SEO\ORM\Yoast_Model;
+
+/**
+ * Table definition for the SEO Meta table.
+ *
+ * @property int    $id
+ * @property string $url
+ * @property int    $post_id
+ * @property int    $target_post_id
+ * @property string $type
+ */
+class SEO_Links extends Yoast_Model {
+
+	/**
+	 * Which columns contain int values.
+	 *
+	 * @var array
+	 */
+	protected $int_columns = [
+		'id',
+		'post_id',
+		'target_post_id',
+	];
+}
