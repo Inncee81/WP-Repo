7db32dee8351aadd0d1ea795121bcd4a1cf829b8
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 13:16:50 -0400

    Initialize jdv/im-289

---
+++ packages/yoastseo/src/helpers/getLanguagesWithWordFormSupport.js
@@ -0,0 +1,17 @@
+import { isFeatureEnabled } from "@yoast/feature-flag";
+
+/**
+ * Checks which languages have morphology support inside YoastSEO.js.
+ *
+ * @returns {string[]} A list of languages that have morphology support.
+ */
+export function getLanguagesWithWordFormSupport() {
+	const supportedLanguages = [ "en", "de", "es", "fr", "it", "nl", "ru", "id", "pt", "pl", "ar", "sv", "he", "hu", "nb", "tr", "cs" ];
+
+	// Add Slovak to the supported languages list if the feature is enabled.
+	if ( isFeatureEnabled( "SLOVAK_SUPPORT" ) ) {
+		supportedLanguages.push( "sk" );
+	}
+
+	return supportedLanguages;
+}
