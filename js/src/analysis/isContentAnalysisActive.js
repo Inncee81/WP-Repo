846490f56b24555d8b105f65966cd680ee1db8d5
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 11:52:31 -0400

    Initialize WPSEO-CS-Cleanup

---
+++ js/src/analysis/isContentAnalysisActive.js
@@ -0,0 +1,16 @@
+var getL10nObject = require( "./getL10nObject" );
+
+import { isUndefined } from "lodash-es";
+
+/**
+ * Returns whether or not the content analysis is active
+ *
+ * @returns {boolean} Whether or not the content analysis is active.
+ */
+function isContentAnalysisActive() {
+	var l10nObject = getL10nObject();
+
+	return ! isUndefined( l10nObject ) && l10nObject.contentAnalysisActive === "1";
+}
+
+module.exports = isContentAnalysisActive;
