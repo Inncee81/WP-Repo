216e205006b1bbdc40dc700d85e82ea6c8360a1d
Inncee81 <58351835+Inncee81@users.noreply.github.com>
2021-07-11 12:17:33 -0400

    Initialize P1-298-image-select-helper

---
+++ tests/unit/helpers/product-helper-test.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Yoast\WP\SEO\Tests\Unit\Helpers;
+
+use Mockery;
+use Yoast\WP\SEO\Helpers\Product_Helper;
+use Yoast\WP\SEO\Tests\Unit\TestCase;
+
+/**
+ * Class Product_Helper_Test
+ *
+ * @coversDefaultClass \Yoast\WP\SEO\Helpers\Product_Helper
+ *
+ * @group helpers
+ */
+class Product_Helper_Test extends TestCase {
+
+	/**
+	 * Represents the instance to test.
+	 *
+	 * @var Mockery\Mock|Product_Helper
+	 */
+	private $instance;
+
+	/**
+	 * Prepares the test.
+	 */
+	protected function set_up() {
+		parent::set_up();
+
+		$this->instance = Mockery::mock( Product_Helper::class )
+			->makePartial()
+			->shouldAllowMockingProtectedMethods();
+	}
+
+	/**
+	 * Retrieves the name when premium is 'active'.
+	 *
+	 * @covers ::get_name
+	 */
+	public function test_get_name_premium() {
+		$this->instance
+			->expects( 'is_premium' )
+			->once()
+			->andReturnTrue();
+
+		$this->assertEquals( 'Yoast SEO Premium plugin', $this->instance->get_name() );
+	}
+
+	/**
+	 * Retrieves the name when premium is not 'active'.
+	 *
+	 * @covers ::get_name
+	 */
+	public function test_get_name_not_premium() {
+		$this->instance
+			->expects( 'is_premium' )
+			->once()
+			->andReturnFalse();
+
+		$this->assertEquals( 'Yoast SEO plugin', $this->instance->get_name() );
+	}
+}
